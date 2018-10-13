d570f0b5c2bc45a9c556019f9a8480b6cf76c7dc
Atif Mustaffa <aretif95@gmail.com>
2018-10-13 21:57:42 +0800

    refactor(serialread.py): Update info

--- serialread.py
+++ serialread.py
@@ -9,7 +9,7 @@ usb_serial_port = "COM3"
 # Website host address
 host= "http://localhost/Water-Quality-Monitoring-System-Website/" # End url with a slash '/'
 
-ser = serial.Serial("COM3",9600)
+ser = serial.Serial(usb_serial_port,9600)
 while True:
 	getVal = ser.readline()
 	val = str(getVal).replace("b'","").replace("\\r\\n'","")
